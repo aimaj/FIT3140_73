25337101c7b6ad503dd573e91ab6cc3727015951
aidan <ahmaj1@student.monash.edu>
2017-03-08 18:40:28 +1100

    removing node modules, adding initial functionality for long/short motions, combining our html solution with the provided bootstrap example control panel

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var led;
 var motion;
 
+var numMotions = 0;
+var motionEnabled = true;
+var longMotions = 0;
+var shortMotions = 0;
+
 function IotDevice() {
 	var five = require("johnny-five");
 	var board = new five.Board();
+	var startTime, endTime = 0;
 	board.on("ready", function() {
 		led = new five.Led(13);
 		motion = new five.Motion(2)
@@ -13,11 +19,30 @@ function IotDevice() {
 	  	});
 	
 	  	motion.on("motionstart", function() {
+			if (motionEnabled == true) {
+				
+			
 	    		console.log("motionstart");
+			socket1.emit('numMotions', ++numMotions);
+			startTime = Date.now();
+			}
 	  	});
 	
 	 	motion.on("motionend", function() {
+	    	if (motionEnabled == true) {
+				console.log("motionend");
+				
+			endTime = Date.now();
+			if ((endTime - startTime < 6000)) {
+				
+			socket1.emit('shortMotions', ++shortMotions);
 	    		console.log("motionend");
+			} else {
+				
+			socket1.emit('longMotions', ++longMotions);
+	    		console.log("motionend");
+			}
+			}
 	  	});
 		this.repl.inject({
 	    		led: led
@@ -49,12 +74,23 @@ var server=http.createServer(function(req, res) {
             console.log('Listening at: http://localhost:8080');
  });
 
-socketio.listen(server).on('connection', function (socket) {
+var socket1 = socketio.listen(server).on('connection', function (socket) {
        socket.on('message', function (msg) {
-           console.log('Message Received: ', msg);
-           socket.broadcast.emit('message', msg);
-		   //when message is received from checkbox, turn on or off the LED.
-		   arduino.toggleLED();
+          	console.log('Message Received: ', msg);
+         if (msg == "toggleLED") {
+			 arduino.toggleLED();
+		 }
+         if (msg == "toggleMotion") {
+			 if (motionEnabled == true) {
+				 motionEnabled = false;
+			 } else {
+				 motionEnabled = true;
+			 }
+          	console.log('togglemotion');
+		 }
+		//when message is received from checkbox, turn on or off the LED.
+		
+		//socket.emit('message', num++);
  });
 });
 
